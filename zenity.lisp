bde43c125fb38e5fc25e1db85f7408986bd749b9
Shinmera <shinmera@tymoon.eu>
2021-09-05 09:04:22 +0200

    Add text input to zenity backend

--- zenity.lisp
+++ zenity.lisp
@@ -8,29 +8,41 @@
 
 (defun zenity (&rest args)
   (handler-case
-      (nth-value 2 (uiop:run-program (list* "zenity" (remove NIL args))
-                                     :ignore-error-status T :external-format :utf-8))
+      (multiple-value-bind (output error status)
+          (uiop:run-program (list* "zenity" (remove NIL args))
+                            :output :string
+                            :ignore-error-status T
+                            :external-format :utf-8)
+        (values status output))
     (error ()
       (error 'messagebox-failed))))
 
 ;; FIXME: figure out whether display is present or not (both X11 and Wayland)
 (define-implementation (text &key title (type :info) modal &allow-other-keys)
-  (case (zenity (ecase type
-                   (:info "--info")
-                   (:error "--error")
-                   (:warning "--warning")
-                   (:question "--question"))
-                 (format NIL "--text=~a" text)
-                 (when title (format NIL "--title=~a" title))
-                 (when modal "--modal"))
-    (0 (case type
-         (:question :yes)
-         (T :ok)))
-    (1 (case type
-         (:question :no)
-         (T :cancel)))
-    (5 (case type
-         (:question :no)
-         (T :cancel)))
-    (T
-     (error 'messagebox-failed))))
+    (multiple-value-bind (status output)
+        (zenity (ecase type
+                  (:info "--info")
+                  (:error "--error")
+                  (:warning "--warning")
+                  (:question "--question")
+                  (:line "--entry")
+                  (:text "--text-info")
+                  (:password "--password"))
+                (format NIL "--text=~a" text)
+                (when (eql type :text) "--editable")
+                (when title (format NIL "--title=~a" title))
+                (when modal "--modal"))
+      (case status
+        (0 (case type
+             (:question :yes)
+             ((:line :text :password)
+              (string-right-trim '(#\Linefeed) output))
+             (T :ok)))
+        (1 (case type
+             (:question :no)
+             (T :cancel)))
+        (5 (case type
+             (:question :no)
+             (T :cancel)))
+        (T
+         (error 'messagebox-failed)))))
